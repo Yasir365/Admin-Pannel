199df4ab913d569400a85ff7011da731dc2f158d
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-05 03:09:38 +0500

    udpate sidebar

--- src/app/admin-pannel/admin-pannel.component.ts
+++ src/app/admin-pannel/admin-pannel.component.ts
@@ -6,20 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-pannel.component.css']
 })
 export class AdminPannelComponent implements OnInit {
+  toggle: any
+  searchBtn: any
+  modeSwitch: any
+  toggleSwitch: any
+  modeText: any
 
+  darkMode = false;
   constructor() { }
 
   ngOnInit() {
-  }
-  isNavbarOpen: boolean = false;
-  activeLink: string = 'Dashboard';
-
-  toggleNavbar() {
-    this.isNavbarOpen = !this.isNavbarOpen;
+    this.toggle = document.querySelector('.toggle');
+    this.searchBtn = document.querySelector('.search-box');
+    this.modeSwitch = document.querySelector('.toggle-swich');
+    this.modeText = document.querySelector('.mode-text');
   }
 
-  setActiveLink(link: string) {
-    this.activeLink = link;
+  toggleDarkMode() {
+    let body = document.querySelector('body');
+
+    if (body?.classList.contains('dark')) {
+      body.classList.remove('dark');
+      this.darkMode = false;
+    } else {
+      body?.classList.add('dark');
+      this.darkMode = true;
+    }
   }
 
+  toggleSidebar() {
+    let sidebar = document.querySelector('.sidebar');
+
+    if (sidebar?.classList.contains('close')) {
+      sidebar.classList.remove('close');
+    } else {
+      sidebar?.classList.add('close');
+    }
+  }
 }
