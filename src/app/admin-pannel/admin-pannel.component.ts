01a96e8d1b43b1f74db1a805e03e2c32812124c2
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-16 10:30:40 +0500

    udpated

--- src/app/admin-pannel/admin-pannel.component.ts
+++ src/app/admin-pannel/admin-pannel.component.ts
@@ -2,8 +2,37 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-pannel',
-  templateUrl: './admin-pannel.component.html',
-  styleUrls: ['./admin-pannel.component.scss']
+  template: `
+  <section class="main">
+    <app-sidebar></app-sidebar>
+    
+    <section class="home">
+      <router-outlet></router-outlet>
+    </section>
+  </section>
+  `,
+  styles: [`@import url('/src/assets/variables.scss');
+
+  .main {
+      min-height: 100vh;
+      background-color: var(--body-color);
+  
+      .home {
+          position: relative;
+          min-height: 100vh;
+          left: 250px;
+          width: calc(100% - 250px);
+          transition: var(--tran-03);
+          padding: 1rem 2rem;
+  
+  
+      }
+  
+      .home.close {
+          left: 78px !important;
+          width: calc(100% - 78px) !important;
+      }
+  }`]
 })
 export class AdminPannelComponent implements OnInit {
 
