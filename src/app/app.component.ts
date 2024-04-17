f407498f37fe65856827925a4dc4993b0743340d
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-17 18:36:26 +0500

    udpated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
 
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    AOS.init({
+      offset: 200,
+      duration: 700,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+  }
   title = 'AdminPannel';
+
 }
