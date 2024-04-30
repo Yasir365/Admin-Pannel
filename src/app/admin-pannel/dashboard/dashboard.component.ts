9a68cfcb29f9a45083403c4a2197a3ed1a75dbba
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-04-30 22:49:43 +0500

    udpate dashbaord

--- src/app/admin-pannel/dashboard/dashboard.component.ts
+++ src/app/admin-pannel/dashboard/dashboard.component.ts
@@ -27,387 +27,117 @@ export class DashboardComponent implements OnInit {
   dropTileId!: number;
   card_items: any;
   chartOptions1: any;
-  chartOptions2: any;
-  chartOptions3: any;
-  chartOptions4: any;
 
   constructor(private adminPannelService: AdminPannelService) {
   }
 
   ngOnInit() {
     this.card_items = [
-      { id: 0, name: "Total Users", value: 45, bg_color: "#DE73FF" },
-      { id: 1, name: "Active Users", value: 30, bg_color: "#24E5A3" },
-      { id: 2, name: "Blocked Users", value: 15, bg_color: "#FCB939" },
-      { id: 3, name: "Total Orders", value: 105, bg_color: "#DE73FF" },
-      { id: 4, name: "Completed Orders", value: 67, bg_color: "#24E5A3" },
-      { id: 5, name: "Pending Orders", value: 38, bg_color: "#FCB939" },
+      { id: 0, name: "Users", value: 1155, bg_color: "#DE73FF" },
+      { id: 1, name: "Categories", value: 2, bg_color: "#24E5A3" },
+      { id: 2, name: "Products", value: 425, bg_color: "#FCB939" },
+      { id: 3, name: "Sold", value: 250, bg_color: "#DE73FF" },
+      { id: 4, name: "Pending", value: 155, bg_color: "#24E5A3" },
+      { id: 5, name: "Cancelled", value: 20, bg_color: "#FCB939" },
     ];
 
 
     this.adminPannelService.getDarkModeData().subscribe((data: any) => {
       if (data) {
-
-        this.chartOptions2 = {
-          series: [105, 67, 38],
-          chart: {
-            height: 430,
-            type: "radialBar",
-            background: '#3A3B3C',
-            showlabel: true
-          },
-          labels: ["Total Orders", "Completed Users", "Pending Orders"],
-        };
-
-
-        this.chartOptions3 = {
+        this.chartOptions1 = {
           series: [
             {
-              name: "Inflation",
-              data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+              name: "Total users",
+              data: [4, 5, 3, 4, 5, 6, 5, 3, 5, 4, 4, 2]
+            },
+            {
+              name: "Acive users",
+              data: [2, 3, 2, 3, 2, 6, 3, 2, 3, 2, 3, 1]
+            },
+            {
+              name: "Blocked users",
+              data: [2, 2, 1, 1, 3, 0, 2, 1, 2, 2, 1, 1]
             }
           ],
           chart: {
             height: 410,
-            type: "bar",
+            type: "line",
+            zoom: {
+              enabled: false
+            },
             background: '#3A3B3C',
-          },
-          plotOptions: {
-            bar: {
-              dataLabels: {
-                position: "top" // top, center, bottom
-              }
-            }
+            borderRadius: 50
+
           },
           dataLabels: {
-            enabled: true,
-            formatter: function (val: any) {
-              return val + "%";
-            },
-            offsetY: -20,
-            style: {
-              fontSize: "12px",
-              colors: ["#304758"]
-            }
+            enabled: false
           },
-
-          xaxis: {
-            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            position: "top",
-            labels: {
-              offsetY: -18
-            },
-            axisBorder: {
-              show: false
-            },
-            axisTicks: {
-              show: false
-            },
-            crosshairs: {
-              fill: {
-                type: "gradient",
-                gradient: {
-                  colorFrom: "#D8E3F0",
-                  colorTo: "#BED1E6",
-                  stops: [0, 100],
-                  opacityFrom: 0.4,
-                  opacityTo: 0.5
-                }
-              }
-            },
-            tooltip: {
-              enabled: true,
-              offsetY: -35
-            }
-          },
-          fill: {
-            type: "gradient",
-            gradient: {
-              shade: "light",
-              type: "horizontal",
-              shadeIntensity: 0.25,
-              gradientToColors: undefined,
-              inverseColors: true,
-              opacityFrom: 1,
-              opacityTo: 1,
-              stops: [50, 0, 100, 100]
-            }
-          },
-          yaxis: {
-            axisBorder: {
-              show: false
-            },
-            axisTicks: {
-              show: false
-            },
-            labels: {
-              show: false,
-              formatter: function (val: any) {
-                return val + "%";
-              }
-            }
+          stroke: {
+            curve: "straight"
           },
           title: {
-            text: "Monthly Inflation in Argentina, 2002",
-            floating: 0,
-            offsetY: 320,
-            align: "center",
-            style: {
-              color: "#444"
-            }
-          }
-        };
-
-
-        this.chartOptions4 = {
-          series: [
-            {
-              name: "Series 1",
-              data: [20, 100, 40, 30, 50, 80, 33]
-            }
-          ],
-          chart: {
-            height: 410,
-            type: "radar",
-            background: '#3A3B3C',
-          },
-          dataLabels: {
-            enabled: true
-          },
-          plotOptions: {
-            radar: {
-              size: 140,
-              polygons: {
-                strokeColor: "#e9e9e9",
-                fill: {
-                  colors: ["#f8f8f8", "#fff"]
-                }
-              }
-            }
-          },
-          title: {
-            text: "Radar with Polygon Fill"
-          },
-          colors: ["#FF4560"],
-          markers: {
-            size: 4,
-            colors: ["#fff"],
-            strokeColors: ["#FF4560"],
-            strokeWidth: 2
-          },
-          tooltip: {
-            y: {
-              formatter: function (val: any) {
-                return val;
-              }
-            }
+            text: "Product Trends by Month",
+            align: "left"
           },
           xaxis: {
-            categories: [
-              "Sunday",
-              "Monday",
-              "Tuesday",
-              "Wednesday",
-              "Thursday",
-              "Friday",
-              "Saturday"
-            ]
-          },
-          yaxis: {
-            tickAmount: 7,
-            labels: {
-              formatter: function (val: any, i: any) {
-                if (i % 2 === 0) {
-                  return val;
-                } else {
-                  return "";
-                }
-              }
-            }
+            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
           }
         };
 
 
       } else {
-        this.chartOptions2 = {
-          series: [105, 67, 38],
-          chart: {
-            height: 430,
-            type: "radialBar",
-            background: '#fff',
-            showlabel: true
-          },
-          labels: ["Total Orders", "Completed Users", "Pending Orders"],
-        };
-
-
-        this.chartOptions3 = {
+        this.chartOptions1 = {
           series: [
             {
-              name: "Inflation",
-              data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+              name: "Users",
+              data: [4, 5, 3, 4, 5, 6, 5, 3, 5, 4, 4, 2]
+            },
+            {
+              name: "Categories",
+              data: [2, 3, 2, 3, 2, 6, 3, 2, 3, 2, 3, 1]
+            },
+            {
+              name: "Products",
+              data: [2, 2, 1, 1, 3, 0, 2, 1, 2, 2, 1, 1]
+            },
+            {
+              name: "Sold",
+              data: [2, 3, 2, 6, 3, 2, 3, 2, 3, 2, 3, 1]
+            },
+            {
+              name: "Pending",
+              data: [2, 2, 1, 1, 1, 2, 2, 1, 1, 3, 0, 2]
+            },
+            {
+              name: "Cancelled",
+              data: [3, 2, 3, 2,  2, 3, 2, 6, 3, 2, 3, 1]
             }
+
           ],
           chart: {
             height: 410,
-            type: "bar",
+            type: "line",
+            zoom: {
+              enabled: false
+            },
             background: '#fff',
-          },
-          plotOptions: {
-            bar: {
-              dataLabels: {
-                position: "top" // top, center, bottom
-              }
-            }
+            borderRadius: 50
+
           },
           dataLabels: {
-            enabled: true,
-            formatter: function (val: any) {
-              return val + "%";
-            },
-            offsetY: -20,
-            style: {
-              fontSize: "12px",
-              colors: ["#304758"]
-            }
+            enabled: false
           },
-
-          xaxis: {
-            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            position: "top",
-            labels: {
-              offsetY: -18
-            },
-            axisBorder: {
-              show: false
-            },
-            axisTicks: {
-              show: false
-            },
-            crosshairs: {
-              fill: {
-                type: "gradient",
-                gradient: {
-                  colorFrom: "#D8E3F0",
-                  colorTo: "#BED1E6",
-                  stops: [0, 100],
-                  opacityFrom: 0.4,
-                  opacityTo: 0.5
-                }
-              }
-            },
-            tooltip: {
-              enabled: true,
-              offsetY: -35
-            }
-          },
-          fill: {
-            type: "gradient",
-            gradient: {
-              shade: "light",
-              type: "horizontal",
-              shadeIntensity: 0.25,
-              gradientToColors: undefined,
-              inverseColors: true,
-              opacityFrom: 1,
-              opacityTo: 1,
-              stops: [50, 0, 100, 100]
-            }
-          },
-          yaxis: {
-            axisBorder: {
-              show: false
-            },
-            axisTicks: {
-              show: false
-            },
-            labels: {
-              show: false,
-              formatter: function (val: any) {
-                return val + "%";
-              }
-            }
+          stroke: {
+            curve: "straight"
           },
           title: {
-            text: "Monthly Inflation in Argentina, 2002",
-            floating: 0,
-            offsetY: 320,
-            align: "center",
-            style: {
-              color: "#444"
-            }
-          }
-        };
-
-
-        this.chartOptions4 = {
-          series: [
-            {
-              name: "Series 1",
-              data: [20, 100, 40, 30, 50, 80, 33]
-            }
-          ],
-          chart: {
-            height: 410,
-            type: "radar",
-            background: '#fff',
-          },
-          dataLabels: {
-            enabled: true
-          },
-          plotOptions: {
-            radar: {
-              size: 140,
-              polygons: {
-                strokeColor: "#e9e9e9",
-                fill: {
-                  colors: ["#f8f8f8", "#fff"]
-                }
-              }
-            }
-          },
-          title: {
-            text: "Radar with Polygon Fill"
-          },
-          colors: ["#FF4560"],
-          markers: {
-            size: 4,
-            colors: ["#fff"],
-            strokeColors: ["#FF4560"],
-            strokeWidth: 2
-          },
-          tooltip: {
-            y: {
-              formatter: function (val: any) {
-                return val;
-              }
-            }
+            text: "Product Trends by Month",
+            align: "left"
           },
           xaxis: {
-            categories: [
-              "Sunday",
-              "Monday",
-              "Tuesday",
-              "Wednesday",
-              "Thursday",
-              "Friday",
-              "Saturday"
-            ]
-          },
-          yaxis: {
-            tickAmount: 7,
-            labels: {
-              formatter: function (val: any, i: any) {
-                if (i % 2 === 0) {
-                  return val;
-                } else {
-                  return "";
-                }
-              }
-            }
+            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
           }
         };
-
       }
     })
 
